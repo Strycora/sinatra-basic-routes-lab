1f67523c21c461fdece6819d8dc4d4c71402af2f
Vikram Chatterjee <strycora@gmail.com>
2020-12-15 10:48:53 -0700

    commit tree 4ecb124bf9b0778aa40166fd01d7fe00b3725e52

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Vikram Chatterjee"
+    end
+
+    get '/hometown' do
+        "My hometown is Brecksville, OH"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Steal by Maribou State"
+    end
 end
